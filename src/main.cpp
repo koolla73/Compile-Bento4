af43d7b4a2e904a4646318db1ce3432b51cda25d
koolla73 <265058808+koolla73@users.noreply.github.com>
2026-03-13 18:50:22 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include "Ap4Decrypt.hpp"
 
 int main(int argc, char *argv[]) {
+
+    if (argc != 3) {
+        exit(-1);
+    }
+
+    const std::string kid = argv[1];
+    const std::string key = argv[2];
     
     return 0;
 }
