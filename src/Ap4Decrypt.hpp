4b368acba7e689f365ecaf11d0cbd0558120d28b
koolla73 <265058808+koolla73@users.noreply.github.com>
2026-03-13 17:46:41 +0000

    Create Ap4Decrypt.hpp

---
+++ src/Ap4Decrypt.hpp
@@ -0,0 +1,25 @@
+#ifndef _AP4_DECRYPT_H_
+#define _AP4_DECRYPT_H_
+
+#include <cstdint>
+#include <cstdlib>
+
+class AP4_Decrypt {
+public:
+	AP4_Decrypt() = delete;
+	~AP4_Decrypt() = delete;
+	AP4_Decrypt(const AP4_Decrypt& other) = delete;
+	AP4_Decrypt& operator=(const AP4_Decrypt& other) = delete;
+	AP4_Decrypt(AP4_Decrypt&& other) = delete;
+	AP4_Decrypt& operator=(AP4_Decrypt&& other) = delete;
+
+	static size_t decryptAndFragment(uint8_t* segmentData, const size_t segmentSize, const char* keyId, const char* key) const;
+
+private:
+	void* operator new(size_t);
+	void* operator new(size_t, void*);
+	void* operator new[](size_t);
+	void* operator new[](size_t, void*);
+};
+
+#endif // !_AP4_DECRYPT_H_
